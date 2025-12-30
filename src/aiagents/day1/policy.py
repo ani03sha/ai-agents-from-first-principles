f391d8c82f20f9984ef1489fa1cc4b635c8a6e34
ani03sha <anirudh03sharma@gmail.com>
2025-12-30 21:42:05 +0530

    Fix broken logic in day1/policy.py

--- src/aiagents/day1/policy.py
+++ src/aiagents/day1/policy.py
@@ -4,9 +4,9 @@ class Policy:
 
     def select_action(self, state: list[int]) -> str:
         x, y, max_x, max_y = state
-        if x < max_x:
+        if x < max_x - 1:
             return "RIGHT"
-        elif y < max_y:
+        elif y < max_y - 1:
             return "DOWN"
 
         raise RuntimeError("No valid actions available")
